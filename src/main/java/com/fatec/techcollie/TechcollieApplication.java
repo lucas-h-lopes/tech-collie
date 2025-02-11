5021ba50dfc41e6c86cf4d14de3f385e47cd98a1
Lucas Henrique Lopes <lucas.hen.lps@gmail.com>
2025-02-11 20:19:40 -0300

    first commit

---
+++ src/main/java/com/fatec/techcollie/TechcollieApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.techcollie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcollieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcollieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/techcollie/TechcollieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.techcollie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcollieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
