3773a692534f830648a35adf7ce68aa55901dd06
Lucas Henrique Lopes <lucas.hen.lps@gmail.com>
2025-02-11 22:49:17 -0300

    create UserMapper for mapping between entity and dto

---
+++ src/main/java/com/fatec/techcollie/web/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.fatec.techcollie.web.mapper;
+
+import com.fatec.techcollie.model.User;
+import com.fatec.techcollie.web.dto.user.UserCreateDto;
+import com.fatec.techcollie.web.dto.user.UserResponseDto;
+
+import java.util.List;
+
+public class UserMapper {
+
+    private UserMapper(){}
+
+    public static User toUser(UserCreateDto dto){
+        return new User(dto);
+    }
+
+    public static UserResponseDto toResponseDto(User user){
+        return new UserResponseDto(user);
+    }
+
+    public static List<UserResponseDto> toResponseDtoList(List<User> users){
+        return users.stream().map(UserMapper::toResponseDto).toList();
+    }
+}
