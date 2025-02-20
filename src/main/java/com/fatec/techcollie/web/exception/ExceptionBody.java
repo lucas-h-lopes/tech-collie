ca6569fdaf96c9dc3ef5a35959e93f24f1351571
lucas henrique lopes <lucas.hen.lps@gmail.com>
2025-02-19 21:02:21 -0300

    refactor(exception-handler): encapsulate logic in constructor

--- src/main/java/com/fatec/techcollie/web/exception/ExceptionBody.java
+++ src/main/java/com/fatec/techcollie/web/exception/ExceptionBody.java
@@ -8,7 +8,9 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -25,29 +27,27 @@ public class ExceptionBody {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Map<String, String> errors;
 
-    public ExceptionBody(HttpServletRequest request, HttpStatus status, String message, Exception exception){
+    public ExceptionBody(HttpServletRequest request, HttpStatus status, String message, Exception exception, boolean isValidationError){
         this.path = request.getRequestURI();
         this.method = request.getMethod();
         this.status = status.value();
         this.statusMessage = status.getReasonPhrase();
         this.message = message;
         this.exception = exception.getClass().getName();
-    }
 
-    public ExceptionBody(HttpServletRequest request, HttpStatus status, String message, Exception exception, BindingResult result){
-        this.path = request.getRequestURI();
-        this.method = request.getMethod();
-        this.status = status.value();
-        this.statusMessage = status.getReasonPhrase();
-        this.message = message;
-        this.exception = exception.getClass().getName();
-        this.errors = new TreeMap<>();
-        addErrors(errors, result);
-    }
-
-    private void addErrors(Map<String,String> errors, BindingResult result){
-        for(FieldError error : result.getFieldErrors()){
-            errors.put(error.getField(), error.getDefaultMessage());
+        if(isValidationError){
+            this.errors = getFieldErrors(exception);
         }
+    }
+
+    private Map<String, String> getFieldErrors(Exception e){
+        HashMap<String,String> result = new HashMap<>();
+        if(e instanceof MethodArgumentNotValidException validationE){
+            for(FieldError error : validationE.getFieldErrors()){
+                result.put(error.getField(), error.getDefaultMessage());
+            }
+            return result;
+        }
+        return null;
     }
 }
--- src/main/java/com/fatec/techcollie/web/exception/GlobalHandler.java
+++ src/main/java/com/fatec/techcollie/web/exception/GlobalHandler.java
@@ -4,7 +4,6 @@ import com.fatec.techcollie.service.exception.UniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,13 +13,19 @@ public class GlobalHandler {
 
     @ExceptionHandler(UniqueViolationException.class)
     public ResponseEntity<ExceptionBody> uniqueViolationException(HttpServletRequest request, UniqueViolationException e){
-        ExceptionBody exceptionBody = new ExceptionBody(request, HttpStatus.CONFLICT, e.getMessage(), e);
+        ExceptionBody exceptionBody = new ExceptionBody(request, HttpStatus.CONFLICT, e.getMessage(), e, false);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionBody);
     }
 
+    @ExceptionHandler()
+    public ResponseEntity<ExceptionBody> badRequestException(HttpServletRequest request, Exception e){
+        ExceptionBody exceptionBody = new ExceptionBody(request, HttpStatus.BAD_REQUEST, e.getMessage(), e, false);
+        return ResponseEntity.badRequest().body(exceptionBody);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionBody> validationException(MethodArgumentNotValidException e, HttpServletRequest request, BindingResult result){
-        ExceptionBody exceptionBody = new ExceptionBody(request, HttpStatus.UNPROCESSABLE_ENTITY, "Validation error(s)", e, result);
+    public ResponseEntity<ExceptionBody> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
+        ExceptionBody exceptionBody = new ExceptionBody(request, HttpStatus.UNPROCESSABLE_ENTITY, "Validation error(s)", e, true);
         return ResponseEntity.status(422).body(exceptionBody);
     }
 }
