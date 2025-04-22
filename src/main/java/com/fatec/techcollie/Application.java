2790b035c01e2034285da26b8ddb6badba78ee0e
Lucas <lucas.hen.lps@gmail.com>
2025-04-22 01:10:18 -0300

    chore: divided .yaml into dev and prod config

--- src/main/java/com/fatec/techcollie/Application.java
+++ src/main/java/com/fatec/techcollie/Application.java
@@ -2,12 +2,17 @@ package com.fatec.techcollie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder sb = new SpringApplicationBuilder(Application.class);
+        sb.properties("spring.profiles.active=dev");
+        sb.properties("server.port=8080");
+
+        sb.run(args);
     }
 
 }
