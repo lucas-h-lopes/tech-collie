31738ca0859238a3f4b1d4740801c7c08bd6a18e
lucas henrique lopes <lucas.hen.lps@gmail.com>
2025-04-26 18:52:35 -0300

    feat: add findByEmail to user repository

--- src/main/java/com/fatec/techcollie/repository/UserRepository.java
+++ src/main/java/com/fatec/techcollie/repository/UserRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer>, QueryByExampleExecutor<User> {
+
+    Optional<User> findByEmail(String email);
 }
